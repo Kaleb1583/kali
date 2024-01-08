1467bb08c556f71b4613905391b95d0d9d214acc
Kaleb <74982167+Kaleb1583@users.noreply.github.com>
2024-01-08 06:23:58 -0600

    Created startKaliLinux.bat

---
+++ startKaliLinux.bat
@@ -0,0 +1,32 @@
+@echo off
+setlocal
+cd ..
+cd ..
+cd Program Files\Oracle\VirtualBox
+
+REM dir
+
+REM ---
+
+
+echo running...
+
+VBoxManage startvm "KaliLinux"
+
+
+choice /n /t 600 /d y >nul
+
+    echo Key was Pressed
+
+    VBoxManage controlvm "KaliLinux" poweroff
+
+    REM - Other ways to power off -
+    REM VBoxManage controlvm "KaliLinux" acpipowerbutton
+    REM VBoxManage controlvm "KaliLinux" savestate
+
+    exit /b 0
+
+
+
+echo done.
+pause >nul
